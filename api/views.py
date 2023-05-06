11a17484436112b987fba2c7ccf04b918c2e33d9
Валентин Горовенко <Punker8381@yandex.ru>
2023-05-06 11:22:31 +0700

    Изменение структуры проекта. Образ v2.0

--- api/views.py
+++ api/views.py
@@ -1,4 +1,3 @@
-from api.filters import TitleFilter
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
@@ -11,9 +10,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.tokens import AccessToken
-from reviews.models import Category, Genre, Review, Title, User
 
+from api.filters import TitleFilter
 from api_yamdb.settings import DOMAIN_NAME
+from reviews.models import Category, Genre, Review, Title, User
 
 from .mixins import ModelMixinSet
 from .permissions import (AnonimReadOnly, IsAdmin,
